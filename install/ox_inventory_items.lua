b0a85dc158b09e8f3690a7b3f71a63e83b137848
DrSnyder <drsnyder86@gmail.com>
2026-05-17 14:31:29 -0400

    Add files via upload

---
+++ install/ox_inventory_items.lua
@@ -0,0 +1,109 @@
+-- Add to ox_inventory/data/items.lua
+
+['ls_package'] = {
+    label = 'Delivery Package',
+    image = 'ls_package.png',
+    weight = 2500,
+    stack = true,
+    close = true,
+    description = 'A package assigned to a Los Santos Freight Co. delivery route.'
+},
+
+['ls_package2'] = {
+    label = 'Delivery Package',
+    image = 'ls_package2.png',
+    weight = 2500,
+    stack = true,
+    close = true,
+    description = 'A boxed package assigned to a Los Santos Freight Co. delivery route.'
+},
+
+['ls_package3'] = {
+    label = 'Delivery Package',
+    image = 'ls_package3.png',
+    weight = 2500,
+    stack = true,
+    close = true,
+    description = 'A sealed package assigned to a Los Santos Freight Co. delivery route.'
+},
+
+['ls_package4'] = {
+    label = 'Delivery Package',
+    image = 'ls_package4.png',
+    weight = 2500,
+    stack = true,
+    close = true,
+    description = 'A heavy package assigned to a Los Santos Freight Co. delivery route.'
+},
+
+['ls_gift_package'] = {
+    label = 'Gift Package',
+    image = 'ls_gift_package.png',
+    weight = 2500,
+    stack = true,
+    close = true,
+    description = 'A special gift package for a Los Santos Freight Co. delivery route.'
+},
+
+['ls_crate'] = {
+    label = 'Freight Crate',
+    image = 'ls_crate.png',
+    weight = 6000,
+    stack = true,
+    close = true,
+    description = 'A heavy freight crate assigned to a box truck delivery route.'
+},
+
+['ls_ammo_crate'] = {
+    label = 'Ammo Crate',
+    image = 'ls_ammo_crate.png',
+    weight = 7000,
+    stack = true,
+    close = true,
+    description = 'A secured ammunition crate assigned to a restricted delivery route.'
+},
+
+['ls_secure_crate'] = {
+    label = 'Secure Government Crate',
+    image = 'ls_secure_crate.png',
+    weight = 7000,
+    stack = true,
+    close = true,
+    description = 'A secured government crate assigned to a Los Santos Freight Co. route.'
+},
+
+['ls_military_crate'] = {
+    label = 'Merryweather Crate',
+    image = 'ls_military_crate.png',
+    weight = 8000,
+    stack = true,
+    close = true,
+    description = 'A restricted military freight crate.'
+},
+
+['ls_military_crate2'] = {
+    label = 'Military Crate',
+    image = 'ls_military_crate2.png',
+    weight = 8000,
+    stack = true,
+    close = true,
+    description = 'A military freight crate assigned to a restricted route.'
+},
+
+['ls_delivery_manifest'] = {
+    label = 'Delivery Manifest',
+    image = 'ls_delivery_manifest.png',
+    weight = 100,
+    stack = false,
+    close = true,
+    description = 'A Los Santos Freight Co. package delivery manifest.'
+},
+
+['ls_trailer_manifest'] = {
+    label = 'Trailer Manifest',
+    image = 'ls_trailer_manifest.png',
+    weight = 100,
+    stack = false,
+    close = true,
+    description = 'A Los Santos Freight Co. trailer delivery manifest.'
+},
---
+++ install/qs_inventory_items.lua
@@ -0,0 +1,19 @@
+-- Quasar Inventory usually supports qb-style shared item definitions.
+-- Add these to your shared items file if your qs-inventory build uses qb item data.
+-- If your build uses a different item format, use the same item names/images from this list.
+
+-- Required item names:
+-- ls_package
+-- ls_package2
+-- ls_package3
+-- ls_package4
+-- ls_gift_package
+-- ls_crate
+-- ls_ammo_crate
+-- ls_secure_crate
+-- ls_military_crate
+-- ls_military_crate2
+-- ls_delivery_manifest
+-- ls_trailer_manifest
+
+-- You can copy the contents of install/qb_inventory_items.lua for most qs-inventory installs.
