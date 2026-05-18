f6aa5a2fba867ccce1dda934c781b2ad8b2cd3d5
DrSnyder <drsnyder86@gmail.com>
2026-05-18 00:27:02 -0400

    Update qb_radial_items.lua

--- install/qb_radial_items.lua
+++ install/qb_radial_items.lua
@@ -1,20 +1,22 @@
 -- QB-RadialMenu examples
--- Add these entries to qb-radialmenu/config.lua under Config.MenuItems.
--- These use the standard QB radial item format: id/title/icon/type/event/shouldClose.
+-- Add these entries to qb-radialmenu/config.lua under Config.JobInteractions
 
-{
-    id = 'ls_trucking_dispatch',
-    title = 'Freight Dispatch',
-    icon = 'truck-fast',
-    type = 'client',
-    event = 'ls_trucking:client:openDispatch',
-    shouldClose = true
-},
-{
-    id = 'ls_trucking_miniui',
-    title = 'Toggle Trucking UI',
-    icon = 'tablet-screen-button',
-    type = 'client',
-    event = 'ls_trucking:client:toggleMiniUI',
-    shouldClose = true
-}
+['trucker'] = {
+        {
+            id = 'ls_trucking_dispatch',
+            title = 'Freight Dispatch',
+            icon = 'truck-fast',
+            type = 'client',
+            event = 'ls_trucking:client:openDispatch',
+            shouldClose = true
+        },
+        {
+            id = 'ls_trucking_miniui',
+            title = 'Toggle Trucking UI',
+            icon = 'tablet-screen-button',
+            type = 'client',
+            event = 'ls_trucking:client:toggleMiniUI',
+            shouldClose = true
+        }
+    },
+
