7ac700afd5344c13b5f0e933ecc5ca42308dcfb2
DrSnyder <drsnyder86@gmail.com>
2026-06-20 23:22:18 -0400

    Delete server directory

--- server/compat.lua
+++
@@ -1,9 +0,0 @@
---[[
-    LS Trucking server compatibility bridge
-    Inventory helper functions live here so config/config.lua only contains simple settings.
-]]
-
-Config.GetTrunkInventoryId = function(plate)
-    local prefix = (Config.Inventory and Config.Inventory.TrunkPrefix) or 'trunk'
-    return ('%s%s'):format(prefix, plate)
-end
