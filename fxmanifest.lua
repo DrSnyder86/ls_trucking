a13e6ca8205d8ad8e58148fcf2bbd59a7a7bcee5
DrSnyder <drsnyder86@gmail.com>
2026-06-20 23:25:39 -0400

    Update fxmanifest for version and description changes

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,28 +1,56 @@
 fx_version 'cerulean'
 game 'gta5'
+this_is_a_map 'yes'
 
 author 'Drsnyder'
-name 'ls_trucking'
-description 'Los Santos Freight Trucking for QB/Qbox'
-version '1.0.0'
+description 'Los Santos Freight Co. Trucker Job'
+version '1.1.0'
 repository 'https://github.com/DrSnyder86/ls_trucking'
+
 
 lua54 'yes'
 
 shared_scripts {
     '@ox_lib/init.lua',
     'config/config.lua',
+    'config/random_events.lua',
+    'config/vehicles.lua',
+    'config/route_trailers.lua',
+    'config/items.lua',
     'config/contracts.lua'
 }
 
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
     'server/compat.lua',
+    'server/ids.lua',
+    'server/framework.lua',
+    'server/contractors.lua',
+    'server/route_summary.lua',
+    'server/depot_vehicles.lua',
+    'server/cargo.lua',
+    'server/routes.lua',
+    'server/admin.lua',
+    'server/dispatch_data.lua',
+    'server/job_blips.lua',
     'server/main.lua'
 }
 
 client_scripts {
     'client/compat.lua',
+    'client/route_history.lua',
+    'client/freight_handoff.lua',
+    'client/receiver_vehicle_controls.lua',
+    'client/spawn_utils.lua',
+    'client/trailer_cargo_props.lua',
+    'client/trailer_cargo_tester.lua',
+    'client/trailer_cargo_editor.lua',
+    'client/trailer_drop_marker.lua',
+    'client/depot_vehicles.lua',
+    'client/route_state.lua',
+    'client/routes.lua',
+    'client/contractor_ui.lua',
+    'client/job_blips.lua',
     'client/main.lua'
 }
 
@@ -31,12 +59,23 @@ ui_page 'html/index.html'
 files {
     'html/index.html',
     'html/style.css',
+    'html/css/*.css',
     'html/app.js',
     'html/sounds/*.wav',
-    -- 'images/.gitkeep',
-    -- 'images/*.png',
-    -- 'images/*.jpg',
-    -- 'images/*.jpeg',
-    -- 'images/*.webp',
-    -- 'inventory_images/*.png'
+    'images/.gitkeep',
+    'images/*.png',
+    'images/*.jpg',
+    'images/*.jpeg',
+    'images/*.webp',
+    'inventory_images/*.png',
+    'data/**/*.meta'
 }
+
+-- Optional custom vehicle metadata streaming
+data_file 'HANDLING_FILE' 'data/**/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
+data_file 'DLCTEXT_FILE' 'data/**/dlctext.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'data/**/contentunlocks.meta'
